06963a43cb816c4e3269a11627ad49815ccc9c93
Joaquin Borggio <joaquin@langchain.dev>
2025-06-19 12:41:39 -0400

    fmt

--- modules/aws/redis/output.tf
+++ modules/aws/redis/output.tf
@@ -1,4 +1,4 @@
 output "connection_url" {
-  value = "redis://${aws_elasticache_cluster.redis.cache_nodes[0].address}:${aws_elasticache_cluster.redis.cache_nodes[0].port}"
+  value     = "redis://${aws_elasticache_cluster.redis.cache_nodes[0].address}:${aws_elasticache_cluster.redis.cache_nodes[0].port}"
   sensitive = true
 }
