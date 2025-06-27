95e2b6b617df0a36ddb6f38125a0430a386a6022
Joaquin Borggio <joaquin@langchain.dev>
2025-06-27 11:53:12 -0400

    fmt

--- modules/azure/redis/outputs.tf
+++ modules/azure/redis/outputs.tf
@@ -1,5 +1,5 @@
 output "connection_url" {
-  value = "rediss://:${azurerm_redis_cache.redis.primary_access_key}@${azurerm_redis_cache.redis.hostname}:6380"
+  value       = "rediss://:${azurerm_redis_cache.redis.primary_access_key}@${azurerm_redis_cache.redis.hostname}:6380"
   description = "Redis connection string using TLS"
   sensitive   = true
 }
