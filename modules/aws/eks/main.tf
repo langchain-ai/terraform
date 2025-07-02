7b66e38a988ec8d7299ea9501b95cd184b6856da
Joaquin Borggio <joaquin@langchain.dev>
2025-07-02 13:40:27 -0400

    fix: gp2 default class module doesnt work if it exists already

--- modules/aws/eks/main.tf
+++ modules/aws/eks/main.tf
@@ -89,21 +89,45 @@ module "eks_blueprints_addons" {
   depends_on = [module.eks, data.aws_eks_cluster_auth.this]
 }
 
-# Create a default storage class for the EKS cluster.
-resource "kubernetes_storage_class" "gp2_default" {
-  metadata {
-    name = "gp2"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
-  }
+# # Create a default storage class for the EKS cluster.
+# resource "kubernetes_storage_class" "gp2_default" {
+#   metadata {
+#     name = "gp2"
+#     annotations = {
+#       "storageclass.kubernetes.io/is-default-class" = "true"
+#     }
+#   }
 
-  storage_provisioner    = "ebs.csi.aws.com"
-  reclaim_policy         = "Delete"
-  volume_binding_mode    = "WaitForFirstConsumer"
-  allow_volume_expansion = true
+#   storage_provisioner    = "ebs.csi.aws.com"
+#   reclaim_policy         = "Delete"
+#   volume_binding_mode    = "WaitForFirstConsumer"
+#   allow_volume_expansion = true
 
-  parameters = {
-    type = "gp2"
-  }
-}
+#   parameters = {
+#     type = "gp2"
+#   }
+# }
+
+# Patch the default storage class for the EKS cluster.
+resource "null_resource" "patch_gp2_default" {
+  depends_on = [
+    module.eks,
+    aws_eks_addon.ebs-csi,
+    data.aws_eks_cluster_auth.this
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      kubectl patch storageclass gp2 \
+        --type merge \
+        -p '{
+          "metadata": {
+            "annotations": {
+              "storageclass.kubernetes.io/is-default-class": "true"
+            }
+          },
+          "allowVolumeExpansion": true
+        }'
+    EOT
+   }
+ }
