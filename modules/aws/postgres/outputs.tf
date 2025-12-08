66a91f9f4e73bbfb266b8b9b4924acff25fa538f
Joaquin Borggio <joaquin@langchain.dev>
2025-12-08 17:12:33 -0500

    update output

--- modules/aws/postgres/outputs.tf
+++ modules/aws/postgres/outputs.tf
@@ -4,6 +4,6 @@ output "connection_url" {
 }
 
 output "iam_connection_url" {
-  description = "Connection URL for IAM authentication (no password)"
-  value       = var.iam_database_user != null ? "postgres://${var.iam_database_user}@${aws_db_instance.this.endpoint}/${aws_db_instance.this.db_name}" : null
+  description = "Connection URL for IAM authentication (no password). Use as POSTGRES_IAM_CONNECTION_URI"
+  value       = var.iam_database_user != null ? "postgresql://${var.iam_database_user}@${aws_db_instance.this.endpoint}/${aws_db_instance.this.db_name}" : null
 }
